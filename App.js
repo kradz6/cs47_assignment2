aa956fda9317fa5d16509456ca7e7036e3cde7f9
Kathryn <kathryn.radziwonski@gmail.com>
2022-01-27 21:28:38 -0800

    in progress

--- App.js
+++ App.js
@@ -30,14 +30,21 @@ export default function App() {
           <Text style={styles.MTL}>
           MTL
           </Text>
+          <Text style={styles.twoMiles}>
+          2 miles away
+          </Text>
         </ImageBackground>
       </View>
       <View style={styles.profileBottom}>
-        <Text style={styles.hotTake}>
-        My hottest take...
-        </Text>
-        <Image source={require('./assets/Icons/player_light.png')} style={styles.player}/>
-        <Image source={require('./assets/Icons/audio_waveform_light.png')} style={styles.audio}/>
+        <View style={styles.profileBT}>
+          <Text style={styles.hotTake}>
+          My hottest take
+          </Text>
+        </View>
+        <View style={styles.profileBB}>
+          <Image source={require('./assets/Icons/player_light.png')} style={styles.player}/>
+          <Image source={require('./assets/Icons/audio_waveform_light.png')} style={styles.audio}/>
+        </View>
       </View>
       <View style={styles.bottomActionItems}>
         <View style={styles.actionImages}>
@@ -45,7 +52,17 @@ export default function App() {
           <Image source={require('./assets/Icons/heart_light.png')} style={styles.actionItems}/>
           <Image source={require('./assets/Icons/messages_light.png')} style={styles.actionItems}/> 
         </View>
-        <View style={styles.actionItems}/>
+        <View style={styles.actionText}>
+          <Text style={styles.smallText}>
+          Discover 
+          </Text>
+          <Text style={styles.smallText}>
+          Matches 
+          </Text>
+          <Text style={styles.smallText}>
+          DMs 
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -54,14 +71,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F3F3F3',
+    backgroundColor: Themes.light.bg,
     justifyContent: 'flex-start', 
     flexDirection: 'column',
   },
   navBar: {
     display: 'flex',
     height: '10%',
-    backgroundColor: '#F3F3F3',
+    backgroundColor: Themes.light.bg,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
@@ -73,7 +90,7 @@ const styles = StyleSheet.create({
   },
   profileTop: {
     display: 'flex',
-    height: '50%',
+    height: '47%',
     width: '90%',
     borderRadius: 50,
     flexDirection: 'row',
@@ -81,72 +98,113 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'blue',
+    // back shadowing
+    shadowRadius: Themes.light.shadows.shadowRadius,
+    shadowColor: Themes.light.shadows.shadowColor,
+    shadowOpacity: Themes.light.shadows.shadowOpacity,
+    shadowOffset: Themes.light.shadows.shadowOffset,
   },
   MTLpic: {
     height: '100%',
     width: '100%',
-    resizeMode:'contain',
+    resizeMode:'cover',
+    borderRadius: 10,
   },
   imageBackMTL: {
     height: '100%',
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 50,
+    borderRadius: 10,
     backgroundColor: 'red',
   },
   MTL: {
-    fontSize: 20,
+    fontSize: 32,
     fontFamily: 'Sydney',
     position: 'absolute',
+    left: 10,
+    top: 5,
+    color: 'white',
+  },
+  twoMiles: {
+    fontSize: 18,
+    fontFamily: 'Sydney',
+    position: 'absolute',
+    left: 10,
+    bottom: 5,
+    color: 'white',
   },
   profileBottom: {
     display: 'flex',
     flexDirection: 'column',
     alignSelf: 'center',
-    height: '17%',
+    height: '20%',
     width: '90%',
-    borderRadius: 50,
+    borderRadius: 30,
     // takes up all the space it can in the main axis pushing the bottom items to the bottom
     marginBottom: 'auto',
-    backgroundColor: 'lightgreen',
-    flexDirection: 'row',
+    backgroundColor: '#FFFFFF',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    flexWrap: 'wrap',
+    marginTop: 20,
+    // back shadowing
+    shadowRadius: Themes.light.shadows.shadowRadius,
+    shadowColor: Themes.light.shadows.shadowColor,
+    shadowOpacity: Themes.light.shadows.shadowOpacity,
+    shadowOffset: Themes.light.shadows.shadowOffset,
+  },
+  profileBT: {
+    display: 'flex',
+    height: '30%',
+    width: '100%',
+    borderRadius: 20,
+    backgroundColor: Themes.light.bgSecondary,
+  },
+  profileBB: {
+    display: 'flex',
+    height: '55%',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+    backgroundColor: Themes.light.bgSecondary,
   },
   hotTake: {
-    fontSize: 32,
+    fontSize: 28,
     fontFamily: 'Sydney',
-    color: 'black',
+    color: Themes.light.text,
     flexDirection: 'column',
     alignSelf: 'flex-start',
+    position: 'absolute',
+    left: 30,
+    top: 10,
   },
   audio: {
     resizeMode: 'contain',
-    height: '40%',
-    width: '60%',
+    height: '50%',
+    width: '70%',
   },
   player: {
     resizeMode: 'contain',
-    height: '40%',
-    width: '40%',
+    height: '80%',
+    width: '15%',
   },
   bottomActionItems: {
     display: 'flex',
     height: '10%',
     flexDirection: "column",
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: 'black',
+    backgroundColor: Themes.light.navigation,
   },
   actionImages: {
     display: 'flex',
-    height: '80%',
+    height: '70%',
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'red',
+    backgroundColor: Themes.light.navigation,
   },
   actionItems: {
     resizeMode: 'contain',
@@ -154,11 +212,21 @@ const styles = StyleSheet.create({
     width: 40,
   },
   actionText: {
-    height: '20%',
+    display: 'flex',
+    height: '30%',
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'pink',
+    position: 'absolute',
+    right: 9,
+    bottom: 5,
+    backgroundColor: Themes.light.text,
+  },
+  smallText: {
+    fontSize: 20,
+    fontFamily: 'Sydney',
+    color: Themes.light.textSecondary,
   },
   navItems: {
     resizeMode: 'contain',
